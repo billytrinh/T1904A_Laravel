cb49d156f453352dd7a9ba5f2f3d48397e181f9a
billytrinh <quanghoa.trinh@gmail.com>
2020-03-16 14:42:55 +0700

    update edit - delete

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -29,4 +29,37 @@ class AdminController extends Controller
         }
         return redirect()->to("admin/category");
     }
+
+    public function categoryEdit($id){
+        $category = Category::find($id);
+        return view("admin.category.edit",['category'=>$category]);
+    }
+
+    public function categoryUpdate($id,Request $request){
+        $category = Category::find($id);
+        $request->validate([ // truyen vao rules de validate
+            "category_name"=> "required|string|unique:category,category_name,".$id
+        ]);
+        try {
+            $category->update([
+                "category_name"=> $request->get('category_name')
+            ]);
+        }catch (\Exception $e){
+            return redirect()->back();
+        }
+        return redirect()->to("admin/category");
+    }
+
+    public function categoryDestroy($id){
+        $category = Category::find($id);
+        try {
+            $category->delete(); // xoa cung // CRUD
+            // xoa mem
+            // them 1 truong status : 0: Inactive; 1: active
+            // chuyen status tu 1 -> 0
+        }catch (\Exception $e){
+            return redirect()->back();
+        }
+        return redirect()->to("admin/category");
+    }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -2,3 +2,8 @@
 Route::get('category',"AdminController@category");
 Route::get('category/create',"AdminController@categoryCreate");
 Route::post('category/store',"AdminController@categoryStore");
+
+Route::get("category/edit/{id}","AdminController@categoryEdit");
+Route::post("category/update/{id}","AdminController@categoryUpdate");
+
+Route::get('category/delete/{id}',"AdminController@categoryDestroy");
